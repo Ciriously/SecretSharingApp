301b4debb98a1d40118aa5c503d0c8c67edd108e
Aditya Mishra <56506651+Ciriously@users.noreply.github.com>
2024-01-15 22:04:37 +0530

    updated homepage responsivness

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useNavigate, Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
-import Secrets from "./Secrets";
 
 const HomePage = () => {
   const { user, isAuthenticated, logout } = useAuth0();
@@ -23,7 +22,7 @@ const HomePage = () => {
   };
 
   return (
-    <div className="font-mono bg-black text-white min-h-screen  flex-col  items-center">
+    <div className="font-mono bg-black text-white min-h-screen flex-col items-center">
       <Navbar
         isAuthenticated={isAuthenticated}
         user={user}
@@ -31,28 +30,33 @@ const HomePage = () => {
       />
 
       {/* SecretsGuardedCounter Component */}
-      <div className="bg-gray-700 p-2 rounded-md justify-center w-full max-w-screen-md m-auto flex items-center h-16 text-center mt-28 mb-24">
-        <h2 className="text-white text-2xl font-mono font-semibold mr-2">
+      <div className="bg-gray-700 p-2 rounded-md justify-center w-full max-w-screen-md m-auto flex items-center h-16 text-center mt-8 md:mt-16 mb-8 md:mb-24">
+        <h2 className="text-white text-lg md:text-2xl font-mono font-semibold mr-2">
           Secrets Safeguarded So Far
         </h2>
-        <p className="text-sky-500 text-2xl font-bold">:{secretsGuarded}</p>
+        <p className="text-sky-500 text-lg md:text-2xl font-bold">
+          :{secretsGuarded}
+        </p>
       </div>
 
       <div className="flex flex-col items-center justify-center flex-grow">
         {/* Title Screen */}
-        <div className="flex items-center mt-14">
-          <div className="text-8xl font-Inter font-extrabold bg-gradient-to-r from-blue-500 to-white text-transparent bg-clip-text">
+        <div className="flex flex-col items-center mt-8 md:mt-14">
+          <div className="text-4xl md:text-8xl font-inter font-extrabold bg-gradient-to-r from-blue-500 to-white text-transparent bg-clip-text">
             Secret Sharing
           </div>
-          <img src="logo.gif" alt="Heart Gif" className="ml-2 h-20 w-30  " />
+          <img
+            src="logo.gif"
+            alt="logo Gif"
+            className="mt-4 md:mt-6 h-24 md:h-44 w-28 md:w-44"
+          />
         </div>
 
         {/* Information */}
         {isAuthenticated ? (
-          <div className="flex flex-col items-center">
-            {/* Description in an invisible-bordered rectangle */}
-            <div className="border border-transparent p-4 rounded-md mt-14 max-w-6xl text-center">
-              <p className="text-2xl font-mono text-slate-400 font-bold">
+          <div className="flex flex-col items-center mt-8 md:mt-2">
+            <div className="border border-transparent p-4 rounded-md mt-8 md:mt-14 max-w-6xl text-center">
+              <p className="text-lg md:text-2xl font-mono text-slate-400 font-bold">
                 Remember when your mom told you to never tell anyone your
                 secrets? Well, guess what? She was wrong! At least when it comes
                 to HushHub. Share your deepest desires, wildest dreams, and most
@@ -66,13 +70,13 @@ const HomePage = () => {
 
             <Link
               to="Secrets"
-              className="text-4xl text-blue-500 mt-10 transition-all duration-300 transform hover:translate-x-2"
+              className="text-2xl md:text-4xl text-blue-500 mt-6 md:mt-10 transition-all duration-300 transform hover:translate-x-2"
             >
               Explore Secrets <span className="ml-2">&#8594;</span>
             </Link>
           </div>
         ) : (
-          <p className="text-lg">
+          <p className="text-lg md:text-xl mt-6 md:mt-10">
             Log in to start sharing your secrets securely.
           </p>
         )}
